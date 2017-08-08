64615229e59ceed503637c075a0eb44462a26c8f
imgss <838632526@qq.com>
2017-08-08 20:25:14 +0800

    eslint

--- get.js
+++ get.js
@@ -2,31 +2,30 @@ var fs = require('fs');
 var https = require('https');
 var http = require('http');
 //有水印
-https.get(`https://cn.bing.com/cnhp/coverstory/`, (res) => {
+https.get('https://cn.bing.com/cnhp/coverstory/', (res) => {
   let info = '';
   res.on('data',(data) =>{
     info += data;
-  })
+  });
   res.on('end',() => {
     let data = JSON.parse(info);
     console.log(data.imageUrl);
     http.get(data.imageUrl, (res) =>{
-      let exist = fs.existsSync('./img/')
+      let exist = fs.existsSync('./img/');
       if (!exist) {
-        fs.mkdirSync('./img')
+        fs.mkdirSync('./img');
       }
       let stream = fs.createWriteStream('./img/' + (new Date()).toLocaleString().split(' ')[0] + 'waterMark.jpg', {
         defaultEncoding: 'binary'
-      })
-      res.setEncoding("binary");
+      });
+      res.setEncoding('binary');
       res.on('data', (data) => {
         stream.write(data);
-      })
+      });
       res.on('end', () => {
-        stream.end()
-        console.log('水印美图下载完成')
-      })
-    })
-  })
-
-})
+        stream.end();
+        console.log('水印美图下载完成');
+      });
+    });
+  });
+});
--- getPicture.js
+++ getPicture.js
@@ -12,26 +12,26 @@ nightmare
   .end()
   .then(function (result) {
     console.log(result);
-    let re = /url\("(.+?)\"\)/
-    re.test(result)
+    let re = /url\("(.+?)"\)/;
+    re.test(result);
     https.get(RegExp.$1,(res) => {
-      let exist = fs.existsSync('./img/')
+      let exist = fs.existsSync('./img/');
       if(!exist){
-        fs.mkdirSync('./img')
+        fs.mkdirSync('./img');
       }
       let stream = fs.createWriteStream('./img/' + (new Date()).toLocaleString().split(' ')[0] + '.jpg',{
         defaultEncoding : 'binary'
-      } )
-      res.setEncoding("binary"); 
+      } );
+      res.setEncoding('binary'); 
       res.on('data',(data) => {
         stream.write(data);
-      })
+      });
       res.on('end', () => {
-        stream.end()
-        console.log('美图下载完成')
-      })
-    })
+        stream.end();
+        console.log('美图下载完成');
+      });
+    });
   })
   .catch(function (error) {
-    console.error('Search failed:', error);
+    console.error('下载失败 请重试', error);
   });
